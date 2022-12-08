8e0917c1ad733a66b0ad0640f9439011368fb6f0
Minkyung-Choi <550005@my.cuhsd.org>
2022-12-08 09:59:57 -0800

    finished coding; work on documentation

--- src/PrintStar.java
+++ src/PrintStar.java
@@ -1,2 +1,29 @@
+import java.util.Scanner;
 public class PrintStar {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int column =0;
+        int row = 0;
+        System.out.println("Do you want to print stars? (yes or no): ");
+        String response = in.nextLine();
+        if(response.equals("no")){
+            System.out.print("Bye!");
+        }else{
+            System.out.print("How many columns of stars?: ");
+            column = in.nextInt();
+            System.out.print("How many rows of stars?: ");
+            row = in.nextInt();
+            Star project = new Star(row, column);
+            boolean existance =false;
+            if(row >1){
+                System.out.print("Would you like a line between each row of stars? (yes or no): ");
+                String lines = in.next();
+                if(lines.equals("yes")){
+                    existance = true;
+                }
+            }
+            project.printStar(existance);
+        }
+
+    }
 }
--- src/Star.java
+++ src/Star.java
@@ -1,4 +1,41 @@
 public class Star {
 
+    private int row;
+    private int columns;
 
+    public Star(int row, int columns){
+        this.row = row;
+        this.columns=columns;
+    }
+
+    public void printStar(boolean line){
+        boolean manyStars = columns>1;
+        for(int i =0; i<row; i++){
+            for(int j =0; j<3; j++){
+                String starSentence = "";
+                if(j==0){
+                    starSentence ="  *  ";
+                }else if(j==1){
+                    starSentence ="*****";
+                }else{
+                    starSentence = " * * ";
+                }
+                if(manyStars){
+                    starSentence = multiplyString(starSentence);
+                }
+                System.out.println(starSentence);
+            }
+            if(line && row>1){
+                System.out.println();
+            }
+        }
+    }
+
+    public String multiplyString(String sentence){
+        String update = sentence;
+        for(int g=0;g<(columns-1);g++){
+            update = update+"\t"+sentence;
+        }
+        return update;
+    }
 }
